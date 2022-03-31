7b97b8fda68816ffc27d2aa1377e65b067ed3f04
Federico Barbieri <federicobarbieri54@gmail.com>
2022-03-31 22:53:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,5 +10,43 @@ const ulNav = document.querySelector('.nav-ul');
 
 hamburger.addEventListener('click', () => {
     ulNav.classList.toggle('open');
+})
 
+// clicking the nav li means we have to make the burger menu disappear
+
+// about li 
+
+let aboutLi = document.querySelector('.about-li');
+
+aboutLi.addEventListener('click', () => {
+    ulNav.classList.toggle('open');
+    
+})
+
+
+// copywriting li
+
+let copyLi = document.querySelector('.copywriting-li');
+
+copyLi.addEventListener('click', () => {
+    ulNav.classList.toggle('open');
+    
+})
+
+// content li 
+
+let contentLi = document.querySelector('.content-li');
+
+contentLi.addEventListener('click', () => {
+    ulNav.classList.toggle('open');
+    
+})
+
+// touch li 
+
+let touchLi = document.querySelector('.touch-li');
+
+touchLi.addEventListener('click', () => {
+    ulNav.classList.toggle('open');
+    
 })
