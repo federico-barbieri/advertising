7644860fb53f20d368988dc50d5b665af87504dd
Federico Barbieri <federicobarbieri54@gmail.com>
2022-04-04 13:14:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -50,3 +50,33 @@ touchLi.addEventListener('click', () => {
     ulNav.classList.toggle('open');
     
 })
+
+
+// window's width
+
+let spanny1 = document.querySelector('.about-li');
+let spanny2 = document.querySelector('.copywriting-li');
+let spanny3 = document.querySelector('.content-li');
+let spanny4 = document.querySelector('.touch-li');
+
+
+
+window.addEventListener('resize', () =>{
+    let windowWidth = innerWidth;
+
+    let ipadQuery = 800;
+
+    if (windowWidth >= ipadQuery){
+        
+        spanny1.innerHTML = spanny1.innerHTML.toUpperCase();
+        spanny2.innerHTML = spanny2.innerHTML.toUpperCase();
+        spanny3.innerHTML = spanny3.innerHTML.toUpperCase();
+        spanny4.innerHTML = spanny4.innerHTML.toUpperCase();
+        
+    } else{
+        spanny1.innerHTML = spanny1.innerHTML.toLowerCase();
+        spanny2.innerHTML = spanny2.innerHTML.toLowerCase();
+        spanny3.innerHTML = spanny3.innerHTML.toLowerCase();
+        spanny4.innerHTML = spanny4.innerHTML.toLowerCase();
+    }
+})
